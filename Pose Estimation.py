31397cd4ea9c5d3ebfccc821f5c08673b292e7c4
Hıdır Eren Gazioğlu <103429971+Hdreren@users.noreply.github.com>
2023-09-27 10:48:03 +0300

    Add files via upload

---
+++ Pose Estimation.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import time
+
+pTime = 0
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(img)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, _ = img.shape
+            cx, cy = int(lm.x*w), int(lm.y*h)
+
+            if id == 0:
+                cv2.circle(img, (cx, cy), 5, (0,0,255), cv2.FILLED)
+
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, "FPS:" +str(int(fps)), (10,45), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
+
+    cv2.imshow("img", img)
+    cv2.waitKey(1)
+
